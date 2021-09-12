669bda08c76901376c6d59e44ab3a32a5aa54205
Mrsazza <shahriarhossain147@gmail.com>
2021-09-13 02:33:00 +0600

    added all assets -> added views -> added tab view in main view

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Sazza on 13/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
